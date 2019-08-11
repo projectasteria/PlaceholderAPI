7680389cde3ea2aead6ec997724ba73da6f3f294
Prateek Vachher <vachh007@umn.edu>
2019-08-11 16:49:11 -0700

    MongoDB Migration

--- scripts/mongodb.py
+++ scripts/mongodb.py
@@ -7,13 +7,14 @@ from flask import session
 from pymongo import MongoClient
 
 if "credentials.json" not in os.listdir('.'):
-    data = {"mongo_username" : os.environ['mongo_username'], "mongo_password": os.environ['mongo_password'], "mongo_ip": os.environ['mongo_ip'], "mongo_port": os.environ['mongo_port']}
+    data = {"mongo_URI" : os.environ['mongo_URI']}
 
 else:
     data = json.load(open("credentials.json", "r"))
 
+client = MongoClient(data["mongo_URI"])
+
 def log(action, ip, username, incoming, outgoing, status):
-    client = MongoClient("mongodb://{}:{}@{}:{}".format(data["mongo_username"], data["mongo_password"], data["mongo_ip"], data["mongo_port"]))
     collection = client.asteria.logs
     entry = {"username":username.lower(), "action":action, "ip": ip, "incoming": incoming, "outgoing":outgoing, "status":status, "timestamp": datetime.datetime.utcnow()}
     collection.insert_one(entry)
@@ -21,7 +22,6 @@ def log(action, ip, username, incoming, outgoing, status):
     return True
 
 def get_list_users():
-    client = MongoClient("mongodb://{}:{}@{}:{}".format(data["mongo_username"], data["mongo_password"], data["mongo_ip"], data["mongo_port"]))
     collection = client.asteria.usercred
     result = list(collection.find({}, {"username": 1}))
     username_list = list(map(lambda x: x['username'], result))
